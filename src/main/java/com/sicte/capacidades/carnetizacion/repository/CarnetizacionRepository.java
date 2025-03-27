4d8dc56b7b7e27bb54a3bd8271cb1041111d2aa4
BryanUtria <brayan.kastel@gmail.com>
2025-03-27 12:08:28 -0500

    se agrega la variable registro sobre la data del aplicativo carnetizacion

--- src/main/java/com/sicte/capacidades/carnetizacion/repository/CarnetizacionRepository.java
+++ src/main/java/com/sicte/capacidades/carnetizacion/repository/CarnetizacionRepository.java
@@ -7,13 +7,23 @@ import com.sicte.capacidades.carnetizacion.entity.Carnetizacion;
 
 @Repository
 public interface CarnetizacionRepository extends CrudRepository<Carnetizacion, String> {
+    public Carnetizacion findByRegistro(String registro);
+
     public Carnetizacion findByCedulaSupervisor(String cedulaSupervisor);
+
     public Carnetizacion findByNombreSupervisor(String nombreSupervisor);
+
     public Carnetizacion findByCedulaTecnico(String cedulaTecnico);
+
     public Carnetizacion findByNombreTecnico(String nombreTecnico);
+
     public Carnetizacion findByTipoCarnet(String tipoCarnet);
+
     public Carnetizacion findBySolicitud(String solicitud);
+
     public Carnetizacion findByFoto(String foto);
+
     public Carnetizacion findBySegmento(String segmento);
+
     public Carnetizacion findByEstado(String estado);
 }
