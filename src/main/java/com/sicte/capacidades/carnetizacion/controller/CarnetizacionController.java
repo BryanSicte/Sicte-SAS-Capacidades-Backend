0998ee7de51f42ae72174364e6991feb86e66d18
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-03-31 14:44:03 -0500

    Se agrega opcion para cargar imagen de carnetizacion

--- src/main/java/com/sicte/capacidades/carnetizacion/controller/CarnetizacionController.java
+++ src/main/java/com/sicte/capacidades/carnetizacion/controller/CarnetizacionController.java
@@ -1,7 +1,9 @@
 package com.sicte.capacidades.carnetizacion.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.sicte.capacidades.drive.GoogleDriveService;
 import com.sicte.capacidades.carnetizacion.entity.Carnetizacion;
 import com.sicte.capacidades.carnetizacion.service.CarnetizacionService;
 
@@ -20,6 +22,8 @@ public class CarnetizacionController {
     @Autowired
     private CarnetizacionService carnetizacionService;
 
+    private final String folderId = "1jMD6UqxKbVqY003qb9xo9kND5gjkcmU5"; //carpeta carnetizacion
+
     @GetMapping("/Registros")
     public ResponseEntity<List<Carnetizacion>> getTodaRegistros() {
         List<Carnetizacion> registros = carnetizacionService.encontrarTodoRegistros();
@@ -31,5 +35,21 @@ public class CarnetizacionController {
             @RequestBody Carnetizacion nuevoRegistro) {
         Carnetizacion registroGuardado = carnetizacionService.guardarRegistro(nuevoRegistro);
         return new ResponseEntity<>(registroGuardado, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/cargarImagen")
+    public ResponseEntity<String> cargarImagen(@RequestParam("file") MultipartFile file, @RequestParam("filename") String filename) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("No se ha seleccionado ningún archivo");
+        }
+
+        try {
+            String fileId = GoogleDriveService.uploadFile(file, filename, folderId);
+
+            return ResponseEntity.ok("Archivo subido con éxito. ID: " + fileId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al subir el archivo");
+        }
     }
 }
