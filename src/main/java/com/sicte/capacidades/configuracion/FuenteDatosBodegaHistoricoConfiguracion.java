5a2ed070bc90abcd1225cf6a233a8e9dc66ff99d
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-04-10 07:44:12 -0500

    se configura nuevamente el bodegaHistoricoConfiguracion

--- src/main/java/com/sicte/capacidades/configuracion/FuenteDatosBodegaHistoricoConfiguracion.java
+++ src/main/java/com/sicte/capacidades/configuracion/FuenteDatosBodegaHistoricoConfiguracion.java
@@ -1,5 +1,8 @@
 package com.sicte.capacidades.configuracion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,7 +22,7 @@ import jakarta.persistence.EntityManagerFactory;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {
-        "com.sicte.capacidades.bodegaHistorico.repository" }, entityManagerFactoryRef = "bodegaHistoricoEntityManager", transactionManagerRef = "bodegaHistoricoTransactionManager")
+    "com.sicte.capacidades.bodegaHistorico.repository"}, entityManagerFactoryRef = "bodegaHistoricoEntityManager", transactionManagerRef = "bodegaHistoricoTransactionManager")
 
 public class FuenteDatosBodegaHistoricoConfiguracion {
 
@@ -32,10 +35,18 @@ public class FuenteDatosBodegaHistoricoConfiguracion {
     @Bean(name = "bodegaHistoricoEntityManager")
     public LocalContainerEntityManagerFactoryBean bodegaHistoricoEntityManagerFactory(
             EntityManagerFactoryBuilder builder, @Qualifier("bodegaHistoricoDataSource") DataSource dataSource) {
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "none"); 
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.format_sql", true);
+
         return builder
                 .dataSource(dataSource)
                 .packages("com.sicte.capacidades.bodegaHistorico.entity")
                 .persistenceUnit("bodegaHistorico")
+                .properties(properties)
                 .build();
     }
 
